8b253f2ea8fa03c78703a7dfaec8b2bdc6b8d9a9
paynes <jan.koscak@gmail.com>
2018-07-21 22:52:26 +0200

    created first app hello_world

---
+++ 01_hello_world/hello_world.py
@@ -0,0 +1,8 @@
+print('-----------------------------------')
+print('          HELLO APP')
+print('-----------------------------------')
+
+user_name = input('What is your name? ')
+greeting = 'Nice to meet you' + user_name
+
+print(greeting)
